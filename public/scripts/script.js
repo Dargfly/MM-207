02ae036cbe0eff9da5eece51015e59a8e8ee1cc8
Dargfly <dargi1000@gmail.com>
2025-01-25 05:24:40 +0100

    The drawn card is now graphically shown on screen Moved Event Listeners down

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,15 +1,11 @@
 "use strict";
 
 const btnNewDeck = document.getElementById("btnNewDeck");
-const btnShowDeck = document.getElementById("btnShowDeck")
+const btnShowDeck = document.getElementById("btnShowDeck");
 const btnShuffleDeck = document.getElementById("btnShuffleDeck");
 const btnDrawCard = document.getElementById("btnDrawCard");
 const inpDeckID = document.getElementById("inpDeckID");
-
-btnNewDeck.addEventListener("click", newDeck);
-btnShowDeck.addEventListener("click", () => showDeck(inpDeckID.value));
-btnShuffleDeck.addEventListener("click", () => shuffleDeck(inpDeckID.value));
-btnDrawCard.addEventListener("click", () => drawCard(inpDeckID.value));
+const imgCard = document.getElementById("imgCard");
 
 //  Generates a deck of cards
 async function newDeck() {
@@ -24,8 +20,8 @@ async function newDeck() {
 //  Shows the whole deck of cards
 async function showDeck(aDeckID) {
   if (aDeckID) {
-		console.log(aDeckID);
-		
+    console.log(aDeckID);
+
     const response = await fetch(`/temp/deck/${aDeckID}`, {
       method: "GET",
     });
@@ -54,8 +50,30 @@ async function drawCard(aDeckID) {
   if (aDeckID) {
     const response = await fetch(`/temp/deck/${aDeckID}/card`);
     const card = await response.json();
-    console.log(card);
+    return card;
   } else {
     console.log("You have to define deck ID");
   }
 }
+
+//	Shows image of chosen card
+function showCardImage(aCard) {
+	console.log(aCard);
+	
+	const value = aCard.value;
+	const suit = aCard.suit;
+  imgCard.src = `imgs/cards/${value}_of_${suit}.png`;
+}
+
+//  Event listeners ----------------------------------------------------------
+btnNewDeck.addEventListener("click", newDeck);
+btnShowDeck.addEventListener("click", () => showDeck(inpDeckID.value));
+btnShuffleDeck.addEventListener("click", () => shuffleDeck(inpDeckID.value));
+btnDrawCard.addEventListener("click", async () => {
+  try {
+    const card = await drawCard(inpDeckID.value);
+    showCardImage(card);
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+});
