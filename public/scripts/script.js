fe5a44f9ce50a87e832a34dfee9abd8599e7305a
Dargfly <dargi1000@gmail.com>
2025-01-26 04:10:42 +0100

    Added a dialog window for informing the user. Updated the script to be easier read, handle errors correctlly and use the new dialog system

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -9,61 +9,120 @@ const imgCard = document.getElementById("imgCard");
 
 //  Generates a deck of cards
 async function newDeck() {
-  const response = await fetch("/temp/deck", {
-    method: "POST",
-  });
-  const deckObject = await response.json();
-  const deckID = deckObject.deck_id;
-  console.log(deckID);
+  try {
+    const response = await fetch("/temp/deck", {
+      method: "POST",
+    });
+
+    //If response from server is not ok, throw an error
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    const deckObject = await response.json();
+    const deckID = deckObject.deck_id;
+    dialogMessage(
+      "Deck created!",
+      "Deck with ID: " + deckID + " has been made"
+    );
+  } catch (error) {
+    dialogMessage("Error", error.message || "An unexpected error occurred.");
+  }
 }
 
 //  Shows the whole deck of cards
 async function showDeck(aDeckID) {
-  if (aDeckID) {
-    console.log(aDeckID);
-
+  if (!aDeckID) {
+    dialogMessage("Error", "You have define deck ID");
+    return
+  }
+  try {
     const response = await fetch(`/temp/deck/${aDeckID}`, {
       method: "GET",
     });
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
     const message = await response.json();
     console.log(message);
-  } else {
-    console.log("You have to define deck ID");
+  } catch (error) {
+    dialogMessage("Error", error.message || "An unexpected error occurred.");
   }
 }
 
 //  Shuffles the deck of cards
 async function shuffleDeck(aDeckID) {
-  if (aDeckID) {
+  //Checks if a deck is is provided
+  if (!aDeckID) {
+    dialogMessage("Error", "You have to define deck ID");
+    return;
+  }
+
+  try {
     const response = await fetch(`/temp/deck/shuffle/${aDeckID}`, {
       method: "PATCH",
     });
+
+    //If response from server is not ok, throw an error
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
     const message = await response.json();
-    console.log(message);
-  } else {
-    console.log("You have to define deck ID");
+    dialogMessage("Successful!", message.data);
+  } catch (error) {
+    dialogMessage("Error", error.message || "An unexpected error occurred.");
   }
 }
 
 //	Draws a random card
 async function drawCard(aDeckID) {
-  if (aDeckID) {
+  if (!aDeckID) {
+    dialogMessage("Error", "You have to define deck ID");
+    return
+  }
+  try {
     const response = await fetch(`/temp/deck/${aDeckID}/card`);
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
     const card = await response.json();
     return card;
-  } else {
-    console.log("You have to define deck ID");
+  } catch {
+    dialogMessage("Error", error.message || "An unexpected error occurred.");
   }
 }
 
 //	Shows image of chosen card
 function showCardImage(aCard) {
-	console.log(aCard);
-	
-	const value = aCard.value;
-	const suit = aCard.suit;
-  imgCard.src = `imgs/cards/${value}_of_${suit}.png`;
+  if (aCard) {
+    console.log(aCard);
+
+    const value = aCard.value;
+    const suit = aCard.suit;
+    imgCard.src = `imgs/cards/${value}_of_${suit}.png`;
+  }
 }
+
+//  Dialog System ----------------------------------------------------------
+const dialog = document.getElementById("dialog");
+const titleDialog = document.getElementById("titleDialog");
+const textDialog = document.getElementById("textDialog");
+const btnCloseDialog = document.getElementById("btnCloseDialog");
+
+function dialogMessage(title, message) {
+  titleDialog.innerText = title;
+  textDialog.innerText = message;
+
+  dialog.showModal();
+}
+
+btnCloseDialog.addEventListener("click", () => {
+  dialog.close();
+});
 
 //  Event listeners ----------------------------------------------------------
 btnNewDeck.addEventListener("click", newDeck);
