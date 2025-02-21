347d3699e240c226d7749489fdd0e3c09da4e082
Eskil <85246824+Dargfly@users.noreply.github.com>
2025-02-21 04:06:19 +0100

    test for file directory

--- init/init.mjs
+++ init/init.mjs
@@ -6,12 +6,20 @@ let treeDummyAmbulance = null
 async function start() {
   treeDummyAmbulance = await fs.readFile("./dummy/TreeAmbulanceDriver.json", "Utf-8");
 
-
   const server = await import("../server.mjs");
 
   const jsonData = JSON.parse(treeDummyAmbulance);
 }
 
+import fs from 'fs/promises';
+
+async function checkFiles() {
+  const files = await fs.readdir('./dummy');
+  console.log(files);
+}
+
+checkFiles();
+
 start(); // Kjører den asynkrone funksjonen
 
-export {treeDummyAmbulance};
+export { treeDummyAmbulance };
