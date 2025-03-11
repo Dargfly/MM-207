4583579bba75858b1cb4464f66d909108cf7b737
Dargfly <dargi1000@gmail.com>
2025-03-11 20:51:31 +0100

    Can now read recipe dummy file

--- init/init.mjs
+++ init/init.mjs
@@ -4,9 +4,10 @@ import { Node, Tree, saveTree, inflateTree } from "../data/tree.mjs";
 // Start asynkron funksjon
 async function start() {
   const server = await import("../server.mjs");
-}
+};
 
 let JSONtreeDummyAmbulance = null;
+let JSONrecipes = null;
 
 (async function loadTree() {
   try {
@@ -21,5 +22,15 @@ let JSONtreeDummyAmbulance = null;
   }
 })();
 
-export { JSONtreeDummyAmbulance };
+(async function loadRecipes() {
+  try {
+    const recipeDummy = await fs.readFile("./dummy/recipes.json", "utf-8");
+    JSONrecipes = JSON.parse(recipeDummy);
+    console.log("Recipes loaded successfully");
+  } catch (error) {
+    console.error("Failed to load recipes:", error);
+  }
+})();
+
+export { JSONtreeDummyAmbulance, JSONrecipes };
 start(); // Kjører den asynkrone funksjonen
