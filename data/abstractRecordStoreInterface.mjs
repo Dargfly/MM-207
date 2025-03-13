e07aab77003da7387d29ff623432c3c3ebeedc2e
Dargfly <dargi1000@gmail.com>
2025-03-13 18:05:46 +0100

    added basic interface for storing data to extend from

---
+++ data/abstractRecordStoreInterface.mjs
@@ -0,0 +1,17 @@
+export default class RecordStoreInterface {
+  async create(id) {
+    throw new Error("WIP - create method not implemented");
+  }
+
+  async read(item) {
+    throw new Error("WIP - read method not implemented");
+  }
+
+  async update(item) {
+    throw new Error("WIP - update method not implemented");
+  }
+
+  async remove(item) {
+    throw new Error("WIP - remove method not implemented");
+  }
+}
