e1c6b916355c622c0dc55d12cc778a41a5970207
Dargfly <dargi1000@gmail.com>
2025-03-14 12:07:33 +0100

    Modified way to contact server

--- public/modules/apiHandler.mjs
+++ public/modules/apiHandler.mjs
@@ -3,6 +3,7 @@ const HTTP_METHODS = {
   POST: "POST",
   PATCH: "PATCH",
   PUT: "PUT",
+  DELETE: "DELETE",
 };
 
 const isPROD = true;
@@ -14,17 +15,34 @@ const BAS_API = isPROD ? BASE_API_PROD : BASE_API_TEST1;
 const API_ENDPOINTS_RECIPE_SERVER = {
   GetRecipes: `${BAS_API}/recipes`,
   GetRecipeById: `${BAS_API}`,
+  UpdateRecipeById: `${BAS_API}`,
+  AddRecipe: `${BAS_API}`,
 
   // DeletNode: (id) => `${BAS_API}/tree/${id}`,
 };
 
 export async function retriveRecipes() {
-  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.GetRecipes);
+  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.GetRecipes, HTTP_METHODS.GET);
   return recipes; // Returner oppskriftene her
 }
 
 export async function retriveRecipeById(id) {
-  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.GetRecipeById+id);
+  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.GetRecipeById+id, HTTP_METHODS.GET);
+  return recipes; // Returner oppskriftene her
+}
+
+export async function updateRecipeById(id, data) {
+  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.UpdateRecipeById+id, HTTP_METHODS.PUT, data);
+  return recipes; // Returner oppskriftene her
+}
+
+export async function deleteRecipeById(id) {
+  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.GetRecipeById+id, HTTP_METHODS.DELETE);
+  return recipes; // Returner oppskriftene her
+}
+
+export async function addRecipe(data) {
+  const recipes = await runRequest(API_ENDPOINTS_RECIPE_SERVER.AddRecipe, HTTP_METHODS.POST, data);
   return recipes; // Returner oppskriftene her
 }
 
@@ -45,7 +63,7 @@ async function runRequest(path, method = HTTP_METHODS.GET, data = null) {
   }
 
   const respons = await fetch(path, request);
-  console.log(respons);
+  // console.log(respons);
 
   return await respons.json();
 }
